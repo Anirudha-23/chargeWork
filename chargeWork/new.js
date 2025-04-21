91410c3a6a452a0c8b45b7fdca7479cc03b5ab0e
Anirudha-23 <swevintern4@chargeworks.in>
2025-04-21 10:51:56 +0530

    Add your file to main branch

---
+++ chargeWork/new.js
@@ -0,0 +1,8 @@
+//to enter array value from the user
+let n=prompt("ENTER THE NUMBER");
+let arr=[];
+for(i=0 ; i< n ; i++){
+let ani=prompt("enter the array number");
+    arr[i]=ani;
+}
+console.log(arr);
